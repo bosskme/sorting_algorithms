705a83c2af7c90992b885a64f339460b57613852
bosskme <kingsley.onele000@gmail.com>
2023-04-26 01:29:29 -0700

    Selection sort

--- 2-selcetion_sort.c
+++ 2-selcetion_sort.c
@@ -6,38 +6,36 @@
   */
 void selection_sort(int *array, size_t size)
 {
-  int i, j, min_index, temp;
-
-  for (i = 0; i < size-1; i++) {
-    min_index = 1;
-
-    for (j = i+1; j < size; j++) {
-      if (array[j] < array[min_index]) {
-	min_index = j;
-      }
-    }
-
-    if (min_index != i) {
-      temp = array[i];
-      array[i] = array[min_index];
-      array[min_index] = temp;
-
-      printf("Swapped elements: %d, %d\n", array[i], array[min_index]);
-    }
-
-    printf("Array after iteration %d: ", i+1);
-    for (int k = 0; k < size; K++) {
-      printf("%d ", array[k]);
-    }
-
-    printf("\n");
-  }
-}
-
-int main()
+int i, j, min_index, temp;
+for (i = 0; i < size - 1; i++)
 {
-  int arr[] = {64, 25, 12, 22, 11};
-  size_t size = sizeof(arr) / sizeof(arr[0]);
-  selection_sort(arr, size);
-  return 0;
+min_index = 1;
+for (j = i + 1; j < size; j++)
+{
+if (array[j] < array[min_index])
+{
+min_index = j;
+}
+}
+if (min_index != i)
+{
+temp = array[i];
+array[i] = array[min_index];
+array[min_index] = temp;
+printf("Swapped elements: %d, %d\n", array[i], array[min_index]);
+}
+printf("Array after iteration %d: ", i + 1);
+for (int k = 0; k < size; k++)
+{
+printf("%d ", array[k]);
+}
+printf("\n");
+}
+}
+int main(void)
+{
+int arr[] = {64, 25, 12, 22, 11};
+size_t size = sizeof(arr) / sizeof(arr[0]);
+selection_sort(arr, size);
+return (0);
 }
